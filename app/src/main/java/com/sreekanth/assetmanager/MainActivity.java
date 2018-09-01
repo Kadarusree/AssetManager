13691aa3b3b02595d1279d893d40bdb7e2a5568a
Srikanth-Dell-PC <ksreekanthyadav@gmail.com>
2018-09-01 10:03:32 +0530

    Second Commit

--- app/src/main/java/com/sreekanth/assetmanager/MainActivity.java
+++ app/src/main/java/com/sreekanth/assetmanager/MainActivity.java
@@ -1,17 +1,19 @@
 package com.sreekanth.assetmanager;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Dialog d ;
+    Dialog d;
     TextView tv_header;
 
     @Override
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         d = new Dialog(this);
-        tv_header= findViewById(R.id.tv_header);
+        tv_header = findViewById(R.id.tv_header);
 
         String text = "<font color=#156177>ASSET</font> <font color=#ffffff>VISIBILITY</font>";
         tv_header.setText(Html.fromHtml(text));
@@ -40,6 +42,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void register(View view) {
         d.setContentView(R.layout.dialog_register);
+        d.show();
+    }
+
+    public void settings(View view) {
+        startActivity(new Intent(getApplicationContext(), Settings.class));
+    }
+
+    public void logout(View view) {
+        d.setContentView(R.layout.dialog_logout);
+        d.show();
+    }
+
+    public void checkout(View view) {
+        d.setContentView(R.layout.dialog_checkout);
+        d.show();
+    }
+
+    public void audit(View view) {
+        d.setContentView(R.layout.dialog_audit);
+        Button locationAudit = d.findViewById(R.id.btn_location_audit);
+        locationAudit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LocationAudit.class));
+            }
+        });
         d.show();
     }
 }
